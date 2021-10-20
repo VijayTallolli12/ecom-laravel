68f18013d0502cd0967ea4548e561263aacd67cd
Vijay Tallolli <vijaytallolli@gmail.com>
2021-10-20 18:47:32 +0530

    Header footer and login page done

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,15 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h5>Footer About</h5>
+            </div>
+            <div class="col-md-4">
+                <h5>Quick Links</h5>
+            </div>
+            <div class="col-md-4">
+                <h5>Social Links</h5>
+            </div>
+        </div>
+    </div>
+</footer>
